b6fe4c50821e94aae0919e7c785260b1d6f09f3d
RAHUL BONEY <rahulboney14@gmail.com>
2025-10-06 09:25:28 +0530

    initial commit

--- test_yolo.py
+++ test_yolo.py
@@ -4,5 +4,5 @@ source_image = 'testimage.jpg'
 results = model(source_image)
 for r in results:
     r.show() 
-
+#hello
 print("Detection complete!")
---
+++ train.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+model =YOLO('yolov8n.pt')
+results = model.train(
+   data='datasets/traffic_data/data.yaml', 
+   epochs=50,                              
+   imgsz=640,                              
+   project='traffic_monitoring_runs',      
+   name='first_aerial_run'                 
+)#hello
+print("Training complete!new model is saved in the 'traffic_monitoring_runs' folder.")
